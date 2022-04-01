535329976e859158a12b3235b5997786f6205ed3
Bui Nam <buikhacnam11@gmail.com>
2022-04-02 06:14:37 +0700

    update

--- src/main/java/com/buinam/userservice/UserServiceApplication.java
+++ src/main/java/com/buinam/userservice/UserServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
-//https://github.dev/getarrays/userservice
+
 @SpringBootApplication
 public class UserServiceApplication {
 
